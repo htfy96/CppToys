3e6101323cc572786b418f9bec9458629c5e177e
Vic Luo <vicluo96@gmail.com>
2015-10-24 12:35:25 +0800

    Add private magic

---
+++ privateMagic/lib.hpp
@@ -0,0 +1,8 @@
+class A
+{
+        private:
+                int member;
+        public:
+                void setMember(int a);
+                A(int a);
+};
---
+++ privateMagic/main.cpp
@@ -0,0 +1,8 @@
+#include "lib.hpp"
+#include <iostream>
+
+int main()
+{
+        A a(42);
+        std::cout << a.member << std::endl;
+}
