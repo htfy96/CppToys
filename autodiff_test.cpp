5727c082471b10cd13705a25d1f2e1958c5f0755
Zheng Luo <lz96@foxmail.com>
2016-09-27 22:23:49 +0800

    Add guide

--- autodiff_test.cpp
+++ autodiff_test.cpp
@@ -10,6 +10,6 @@ int main()
     using namespace adiff;
     using p1 = Product<Plus< Number<3>, Plus< Var<VarName::x>, Number<42> > > , Number<15> >;
     using p2 = ConstantFold<p1>::type;
-    cout << typeid(p2).name() << endl;
+    cout << typeid(p2).name() << endl;  // ./autodiff_test | c++filt -t
 }
 
